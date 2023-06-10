fe02e39683ac963f0cec4ad31180c488ba3c8cb4
Arif Babayev <arifrb@code.edu.az>
2023-06-10 18:45:00 +0400

    Create endpoint for products

--- controller/productController.js
+++ controller/productController.js
@@ -1,7 +1,14 @@
 const Product = require("../models/ProductModel")
 
-const getProduct = (req,res)=>{
-    res.send("Handling product routes, e.g. search for products")
+const getProduct = async (req,res, next)=>{
+    try {
+        const products = await Product.findOne({}).sort({name:1})
+        // 1-->Ascending; -1-->Descending
+        res.json({products});
+    } catch (error) {
+        next(error)
+    }
+
 }
 
 module.exports = getProduct;
