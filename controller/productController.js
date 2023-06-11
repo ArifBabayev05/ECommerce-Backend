8df9100fa59a05e7d133a7d89c331de71ed13d5f
Arif Babayev <arifrb@code.edu.az>
2023-06-11 23:18:13 +0400

    Pagination system

--- controller/productController.js
+++ controller/productController.js
@@ -1,8 +1,9 @@
+const recordsPerPage = require("../config/pagination");
 const Product = require("../models/ProductModel")
 
 const getProduct = async (req,res, next)=>{
     try {
-        const products = await Product.findOne({}).sort({name:1})
+        const products = await Product.find({}).sort({name:1}).limit(recordsPerPage)
         // 1-->Ascending; -1-->Descending
         res.json({products});
     } catch (error) {
